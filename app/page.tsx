b1d4672d9485e17a740377d09fff2ebd7156e659
Luna <javaspringtea@gmail.com>
2024-08-03 21:05:18 +0800

    add og image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 import {Chatroom} from "@/app/Chatroom";
-import {Metadata} from "next";
 
-export const metadata: Metadata = {
-    title: 'Chatroom',
-    description: 'feel free to talk',
-}
+
 
 export default function Home() {
     return <div id='__next'><Chatroom/></div>
