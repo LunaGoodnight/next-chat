fb8a71a1bcc66ef0aa03f45e1c0815f151cb5b1b
Luna <javaspringtea@gmail.com>
2024-08-03 17:51:57 +0800

    fix css class

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://catsheue-001-site5.atempurl.com/api/:path*', // Correctly include the path pattern
-            },
-        ]
-    },
-};
+const nextConfig = { output: 'export',};
 
 export default nextConfig;
